3d3ef90bc25bf1debdcc885e80489cffa6cf0ce7
Cgeorge1807 <christian03george@gmail.com>
2025-01-29 17:24:41 +0000

    TodoList proceso

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [lista, setLista] = useState(["Make the bed", "Wash my hands", "Eat", "Walk the dog"])
+	const [tareas, setTareas] = useState("")
+
+	const agregar = (evento) => {
+		evento.preventDefault()
+		console.log(evento.key)
+		if (evento.key === "Enter") {
+			console.log(tareas)
+			setLista([...lista, tareas])
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center">
+			<h1 className="text-center mt-5">TodoList</h1>
+			<form className="row g-3 d-flex justify-content-center">
+				<div className="">
+					<input
+						value={tareas}
+						onChange={(evento) => setTareas(evento.target.value)}
+						onKeyDown={agregar}
+						type="text"
+						className="form-control border border-secondary"
+						placeholder="add a task" />
+				</div>
+			</form>
+			<ul className="list-group border-secondary">
+				{
+					lista.map((tarea, index) => (
+						<li key={index} className="list-group-item border border-secondary">
+							{tarea}
+						</li>
+					))
+				}
+			</ul>
 		</div>
 	);
 };
